709a31cebbea474fb556c4645079d676397a1d48
RTSaitama <rtsaitama@outlook.com>
2025-05-20 17:16:49 +0300

    changed tab.id => activeTabId

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,36 +4,34 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
   return (
-    <>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              data-cy="Tab"
-              className={cn({
-                'is-active': tab.id === activeTab.id,
-              })}
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            key={tab.id}
+            data-cy="Tab"
+            className={cn({
+              'is-active': tab.id === activeTabId,
+            })}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                if (tab.id !== activeTabId) {
+                  onTabSelected(tab.id);
+                }
+              }}
             >
-              <a
-                href={`#${tab.id}`}
-                data-cy="TabLink"
-                onClick={() => {
-                  if (tab.id !== activeTab.id) {
-                    onTabSelected(tab.id);
-                  }
-                }}
-              >
-                {tab.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
 
       <div className="block" data-cy="TabContent">
         {activeTab.content}
       </div>
-    </>
+    </div>
   );
 };
